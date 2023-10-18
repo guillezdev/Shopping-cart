73674cd7c0f5764fe03002ccdbe523512bf3be44
guillezdev <guillezdev@gmail.com>
2023-10-18 16:14:47 -0400

    first JS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const carrito = document.querySelector("#carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+const contenedorCarrito = document.querySelector("#lista-carrito");
+const vaciarCarrito = document.querySelector("#vaciar-carrito")
+
+
+console.log(carrito);
+console.log(listaCursos);
+console.log(contenedorCarrito);
+console.log(vaciarCarrito);
